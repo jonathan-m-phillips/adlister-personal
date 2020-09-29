c3b66f8c90e181c3102dd28015b9294a411ab3e5
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-09-29 09:34:58 -0500

    setting up config package

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -1,7 +1,10 @@
 package com.codeup.adlister.dao;
 
+import com.codeup.adlister.config.Config;
 import com.codeup.adlister.models.Ad;
 import com.mysql.cj.jdbc.Driver;
+import com.codeup.adlister.config.Config;
+
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -25,6 +28,7 @@ public class MySQLAdsDao implements Ads {
             throw new RuntimeException("Error connecting to the database!", e);
         }
     }
+
 
     @Override
     public List<Ad> all() {
