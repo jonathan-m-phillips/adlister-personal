d7a5cc914a250e8250502488c01a8891c3590f57
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-09-29 15:24:37 -0500

    updating mysql db

--- migration.sql
+++ migration.sql
@@ -19,8 +19,7 @@ CREATE TABLE ads (
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(240) NOT NULL,
     description TEXT NOT NULL,
-    date DATE NOT NULL,
-    time TIMESTAMP NOT NULL,
+    date DATETIME default now(),
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
